c7c20bcd1286bbcbc2545a76f643718a0427b2df
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-06-29 00:19:19 +0200

    downloaded jinj2 package

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
+app = FastAPI()
+
+
 posts: list[dict] = [
     {
         "id": 1,
@@ -18,12 +21,12 @@ posts: list[dict] = [
     }
 ]
 
-app = FastAPI()
-@app.get("/", response_class=HTMLResponse)
+
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
 def home():
     return f"<h1>{posts[0]['title']}</h1>"
     #return f"<h1>{posts[0]['title']}</h1>"
-
 
 
 @app.get("/api/posts")
